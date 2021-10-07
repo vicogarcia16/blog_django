7aeb1241be1d967f8966d2150af23c4fab0a6ef3
Victor Manuel Garcia Negrete <mg_619@hotmail.com>
2021-10-07 17:04:19 -0500

    almacenaco todos los cambios

--- user/api/router.py
+++ user/api/router.py
@@ -3,5 +3,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
     path('auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh',TokenRefreshView.as_view(), name='token_refresh')
+    path('auth/token/refresh',TokenRefreshView.as_view(), name='token_refresh')#rutas
 ]
